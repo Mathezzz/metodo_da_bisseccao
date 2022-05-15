aeefe557535ffac56d4bee63c3c5fa69f8250ed6
Mathezzz <andersonmmelo23@outlook.com>
2022-05-14 22:28:41 -0300

    Corrigindo bug com intervalos da funcao

--- Biseccao.py
+++ Biseccao.py
@@ -4,11 +4,11 @@ import numpy as np
 
 #Definindo a função matemática que será analisada
 #Função testada:
-def f(x): return x**2-9
-
+#def f(x): return x**3 -6*x
+def f(x): return x**2-3.9
 #Definindo os valores do intervalo
-intervalo_inferior = -5
-intervalo_superior = 5
+intervalo_inferior = -2.0
+intervalo_superior = 2.0
 step=0.5
 
 e=0.0001  #precisão desejada na solução
@@ -25,7 +25,7 @@ def aproximador(valor1, valor2):
     iteracao=0
     while (abs((valor2-valor1)/2)>e):
         xi=(valor1+valor2)/2                        #método da bisecção
-        print("Iteração:", iteracao+1, " xi:", xi)  #Comentar esta linha caso só queira o resultado final da raiz
+        #print("Iteração:", iteracao+1, " xi:", xi)  #Descomentar esta linha caso ver o valor a cada iteração
         iteracao=iteracao+1
         if (iteracao==limite):
             break
@@ -41,17 +41,20 @@ def aproximador(valor1, valor2):
 
 #/Separação de intervalos de bolzano
 raiz_no_ponto=None
-for ponto in np.arange(a, b+c, c):
-    if f(a)==0 and a != raiz_no_ponto:
-        print("intervalo:", a, ponto)
-        print("a raiz e", a)
+for ponto in np.arange(a, b, c):
     if f(ponto)==0:
-        raiz_no_ponto = ponto
-        print("intervalo:", a, ponto)
-        print("a raiz e", raiz_no_ponto)
+        print("intervalo:", a, " <-> ", ponto, "  |  Raiz: ", a)
+    #if f(ponto)==0:
+    #   raiz_no_ponto = ponto
+    #    print("intervalo: ", a, " <-> ", ponto, "  |  Raiz: ", raiz_no_ponto)
     else:
         if f(a)*f(ponto)<0:                 #Se o teorema verdadeiro
-            print("intervalo:", a, ponto)
             raiz=aproximador(a,ponto)       #A raiz é o valor retornado da função aproximação com o intervalo como parâmetro
-            print("a raiz e", raiz)
+            print("intervalo: ", a, " <-> ", ponto, "  |  Raiz aprox: ", raiz)
+    print(a, ponto)
     a=ponto
+if f(ponto)*f(b)<0:                 #Se o teorema verdadeiro
+            raiz=aproximador(ponto, b)       #A raiz é o valor retornado da função aproximação com o intervalo como parâmetro
+            print("intervalo: ", ponto, " <-> ", b, "  |  Raiz aprox: ", raiz)
+if f(b)==0:
+        print("intervalo:", ponto, " <-> ", b, "  |  Raiz: ", b)
